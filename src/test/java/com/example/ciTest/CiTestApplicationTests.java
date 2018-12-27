0e6799ee7d9b3af532e4aa40482a453e8ec15e90
fanliang <fanliang@ppdai.com>
2018-12-28 07:37:13 +0800

    testCode

---
+++ src/test/java/com/example/ciTest/CiTestApplicationTests.java
@@ -0,0 +1,26 @@
+package com.example.ciTest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(properties = "spring.redis.host = redis")
+public class CiTestApplicationTests {
+
+	@Autowired
+	private RedisTemplate redisTemplate;
+
+	@Test
+	public void contextLoads() {
+		for (int i = 0; i < 50000; i++) {
+			System.out.println(i);
+			redisTemplate.opsForValue().set(String.valueOf(i), String.valueOf(i));
+		}
+	}
+
+}
+
